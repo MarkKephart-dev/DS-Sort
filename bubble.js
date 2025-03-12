9b105db2ceaa60a51cfc743d231836d7a34063e7
MarksmanX <mkeph561@gmail.com>
2025-03-12 14:19:11 -0400

    Finished problems

--- bubble.js
+++ bubble.js
@@ -1,3 +1,19 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    let i = 0;
+    let swapped = true;
+    while(swapped){
+        swapped = false;
+        for(let j = 0; j < arr.length - 1 - i; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }  
+        }
+        i++;
+    }
+    return arr;
+}
 
 module.exports = bubbleSort;
